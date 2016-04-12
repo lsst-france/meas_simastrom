35a0e154d96a5c6553480159d089642a0edf7b42
Pierre Astier <pierre.astier@in2p3.fr>
2016-04-12 18:20:42 +0200

    added ChipArrangement class

---
+++ include/lsst/meas/simastrom/ChipArrangement.h
@@ -0,0 +1,75 @@
+#ifndef CHIPARRANGEMENT__H
+#define CHIPARRANGEMENT__H
+
+#include <string>
+#include <map>
+//#include <vector>
+
+
+#include "lsst/meas/simastrom/Gtransfo.h"
+#include "lsst/meas/simastrom/Frame.h"
+#include "lsst/meas/simastrom/CountedRef.h"
+
+namespace lsst {
+namespace meas {
+namespace simastrom {
+
+
+//! The class that exposes mappings from pixel coordinates to the tangent plane for a CCD mosaic. 
+/*! It is meant to be derived for an actual mapping, such as the next classes. These mappings
+are constructed from tools/averagewcs.cc, which can be consulted. */
+class ChipArrangement
+{
+  std::string name;
+  
+ public :
+  //!
+  const std::string &InstrumentName() const {return name;}
+
+  //! The actual transformations.
+  virtual const Gtransfo& Pix2TP(const unsigned Chip) const = 0;
+  //! Frame in the tangent plane w.r.t a conventional optical axis
+
+  //! the size of the instrument.
+  virtual Frame TangentPlaneFrame() const =0;
+
+
+};
+
+//! A concrete implementation with polynomial mappings from CCD to TP coordinates
+class PolyMappingArrangement : public ChipArrangement
+{
+  std::vector<GtransfoPoly> pix2TP;
+  const unsigned nchips;
+  Frame tangentPlaneFrame;
+  
+public:
+  PolyMappingArrangement(const std::string &FileName, const unsigned NChips=0);
+
+  virtual Frame TangentPlaneFrame() const
+  {return tangentPlaneFrame;}
+  
+  const Gtransfo& Pix2TP(const unsigned Chip) const;
+  
+};
+
+
+
+
+typedef std::map<unsigned,CountedRef<Gtransfo > > ChipTransfosType;
+
+/* One way to describe the chip arrangement in the focal plane is to
+map the Pixel coordinates of each chip on the tangent plane.  this is
+described through transformations that can be written to and read from
+files.  Here are the utilities for that: */
+
+//! Read a set of transfos from a file. I guess it is useless to expose it
+ChipTransfosType ReadTransfoFile(const std::string &FileName, Frame& TPFrame);
+
+//! Write a set of transfos to a file.
+void WriteTransfoFile(const std::string &FileName, const ChipTransfosType &V, const Frame & TPFrame); 
+
+}}} // end of namespaces
+
+#endif /* CHIPARRANGEMENT__H */
+
