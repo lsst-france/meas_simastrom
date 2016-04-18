2bd4ca75497f421383b83dc56b794507632b83d4
Pierre Astier <pierre.astier@in2p3.fr>
2016-04-18 17:43:29 +0200

    added saturation flag to ExposureStar

--- include/lsst/meas/simastrom/ExposureCatalog.h
+++ include/lsst/meas/simastrom/ExposureCatalog.h
@@ -20,18 +20,23 @@ class Gtransfo;
 class Frame;
 
 
+#ifndef SWIG
 
 //! just a container for both the coordinates in the tangent plane and the original measured object. 
 class ExposureStar : public BaseStar
 {
  public:
   int chip; // chip ID;
+  bool is_saturated;
+
   const BaseStar *original; // the untransformed object (i.e. the object as read in the calexp catalog)
 
   //!
- ExposureStar(const BaseStar* S, const int Chip) : BaseStar(*S),  chip(Chip), original(S) {};
+ ExposureStar(const BaseStar* S, const int Chip, bool IsSaturated=false) :  BaseStar(*S),  chip(Chip), is_saturated(IsSaturated), original(S) {};
 
 };
+
+#endif
 
 
 typedef StarList<ExposureStar> ExposureStarList;
@@ -50,11 +55,13 @@ class ExposureCatalog
   //!
   void AddCalexp(const lsst::afw::table::SortedCatalogT<lsst::afw::table::SourceRecord> &Cat, const int Chip);
 
+#ifndef SWIG
   //! Assembles the exposure catalog (coordinates in degrees in TP)
   void TangentPlaneCatalog(ExposureStarList &Catalog);
 
   //! the catalog (in pixel coordinates) of a single chip as read (and possibly selected).
   const BaseStarList *ChipCatalog(const int Chip) const;
+#endif
 
   //! ChipArrangement mostly contains the mappings from pixels to tangent plane
   const ChipArrangement& Arrangement() const { return *arrangement;}
